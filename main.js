59fb7f53c6d04e9e6c6e5940b80c59046e479bcc
Toth Avar <toth.avar@gmail.com>
2022-10-16 21:34:32 +0200

    asd

--- main.js
+++ main.js
@@ -9,58 +9,58 @@ const objects = {
     phonkObject : [
         {
             name:   "Cruel",
-            url:    "music/Phonk/Cruel.mp3",
+            url:    "./music/Phonk/Cruel.mp3",
         },
         {
             name:   "Ragnarok",
-            url:    "music/Phonk/Ragnarok.mp3",
+            url:    "./music/Phonk/Ragnarok.mp3",
         },
         {
             name:   "Berserk",
-            url:    "music/Phonk/Berserk.mp3",
+            url:    "./music/Phonk/Berserk.mp3",
         },
         {
             name:   "Wrath",
-            url:    "music/Phonk/Wrath.mp3",
+            url:    "./music/Phonk/Wrath.mp3",
         },
         {
             name:   "Immaculate",
-            url:    "music/Phonk/Immaculate.mp3",
+            url:    "./music/Phonk/Immaculate.mp3",
         },
         {
             name:   "Shadow",
-            url:    "music/Phonk/Shadow.mp3",
+            url:    "./music/Phonk/Shadow.mp3",
         },
         {
             name:   "Midnight",
-            url:    "music/Phonk/Midnight.mp3",
+            url:    "./music/Phonk/Midnight.mp3",
         },
         {
             name:   "Murder In My Mind",
-            url:    "music/Phonk/MIMM.mp3",
+            url:    "./music/Phonk/MIMM.mp3",
         },
         {
             name:   "Prince Of Darkness",
-            url:    "music/Phonk/Prince.mp3",
+            url:    "./music/Phonk/Prince.mp3",
         },
         {
             name:   "Avoid Me",
-            url:    "music/Phonk/Avoid.mp3",
+            url:    "./music/Phonk/Avoid.mp3",
         },
         {
             name:   "S.X.N.D. N.X.D.E.S.",
-            url:    "music/Phonk/SendNudes.mp3",
+            url:    "./music/Phonk/SendNudes.mp3",
         },
         {
             name:   "Overdose",
-            url:    "music/Phonk/Overdose.mp3",
+            url:    "./music/Phonk/Overdose.mp3",
         },
     ],
     
     hardObject : [
         {
             name: "BassSlut",
-            url: "music/Hard/BassSlut.mp3",
+            url: "./music/Hard/BassSlut.mp3",
         },
     ], 
 };
@@ -70,7 +70,7 @@ music.volume = 0.1;
 
 let selectedObject;
 let randomiser
-/* Defining the music selecter function */
+/* Defining the ./music selecter function */
 
 const musicSelect = function(selectedObject) {
     const  rand = Math.floor(Math.random() * selectedObject.length);
@@ -81,7 +81,7 @@ const musicSelect = function(selectedObject) {
     document.getElementById("Title").innerHTML = "Playing: "+ selectedObject[rand].name;
 }
 
-const randomMusic = function() {
+const randommusic = function() {
     let keys = Object.keys(objects);
     const rand = Math.floor(Math.random() * keys.length);
     const randomObject = objects[keys[rand]];
@@ -93,20 +93,20 @@ const randomMusic = function() {
     document.getElementById("Title").innerHTML = "Playing: "+ randomObject[randommMusic].name;
 }
 
-/* Button that plays new music */
+/* Button that plays new ./music */
 
 button.onclick = function(){
-    randomMusic();
+    randommusic();
     selectedObject = randomiser
 }
 
-/* If music ends calling the random music selecter and player function */
+/* If ./music ends calling the random ./music selecter and player function */
 
 music.addEventListener('pause', function(){
     if (selectedObject != randomiser)
         musicSelect(selectedObject);
     else
-        randomMusic();
+        randommusic();
 });
 
 /* Adding Buttons (In Progress) */
