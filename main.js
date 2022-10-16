9652525804a67dd09e1a3a98b35e7971771e7ce5
Toth Avar <toth.avar@gmail.com>
2022-10-16 17:18:59 +0200

    Tweaking

--- main.js
+++ main.js
@@ -1,49 +1,59 @@
 /* Defining things */
+
 const  music = document.getElementById("BackgroundM");
 const  source = document.getElementById("source");
 const  button = document.getElementById("myButton")
 const  musicObject = [
     {
         name:   "Cruel",
-        url:    "/music/Cruel.mp3",
+        url:    "/music/Phonk/Cruel.mp3",
     },
     {
         name:   "Ragnarok",
-        url:    "/music/Ragnarok.mp3",
+        url:    "/music/Phonk/Ragnarok.mp3",
     },
     {
         name:   "Berserk",
-        url:    "/music/Berserk.mp3",
+        url:    "/music/Phonk/Berserk.mp3",
     },
     {
         name:   "Wrath",
-        url:    "/music/Wrath.mp3",
+        url:    "/music/Phonk/Wrath.mp3",
     },
     {
         name:   "Immaculate",
-        url:    "/music/Immaculate.mp3",
+        url:    "/music/Phonk/Immaculate.mp3",
     },
     {
-        name:   "BassSlut",
-        url:    "/music/BassSlut.mp3",
-    }
+        name:   "Shadow",
+        url:    "/music/Phonk/Shadow.mp3",
+    },
+    {
+        name:   "Midnight",
+        url:    "/music/Phonk/Midnight.mp3",
+    },
 ]
 music.volume = 0.1;
 
 /* Selecting random music from array function */
+
 function musicSelect() {
+    /*
     if(music.paused) {
+    */
         const  rand = Math.floor(Math.random() * musicObject.length)
         source.src = musicObject[rand].url
         music.load();
         music.play();
         console.log("Playing " + musicObject[rand].name)
-    } 
+     
 }
+
 /* Detecting user interaction to prevent user didn't interact with document error */
 
+document.addEventListener("click", musicSelect)
 
 /* If music ends calling the random music selecter and player function */
 music.addEventListener('pause', musicSelect)
-document.addEventListener("click", musicSelect)
+
 
