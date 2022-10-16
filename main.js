781efb235b259d1f4e05734b027c2003b5c0532e
Toth Avar <toth.avar@gmail.com>
2022-10-16 19:16:32 +0200

    Changed everythingg
    added buttons , new music, remade scripts

--- main.js
+++ main.js
@@ -2,85 +2,116 @@
 
 const  music = document.getElementById("BackgroundM");
 const  source = document.getElementById("source");
-const  button = document.getElementById("myButton");
-const phonkButton = document.getElementById("phonkButton")
+const  button = document.getElementById("playButton");
+const phonkButton = document.getElementById("phonkButton");
+const hardButton = document.getElementById("hardButton");
+const objects = {
+    phonkObject : [
+        {
+            name:   "Cruel",
+            url:    "/music/Phonk/Cruel.mp3",
+        },
+        {
+            name:   "Ragnarok",
+            url:    "/music/Phonk/Ragnarok.mp3",
+        },
+        {
+            name:   "Berserk",
+            url:    "/music/Phonk/Berserk.mp3",
+        },
+        {
+            name:   "Wrath",
+            url:    "/music/Phonk/Wrath.mp3",
+        },
+        {
+            name:   "Immaculate",
+            url:    "/music/Phonk/Immaculate.mp3",
+        },
+        {
+            name:   "Shadow",
+            url:    "/music/Phonk/Shadow.mp3",
+        },
+        {
+            name:   "Midnight",
+            url:    "/music/Phonk/Midnight.mp3",
+        },
+        {
+            name:   "Murder In My Mind",
+            url:    "/music/Phonk/MIMM.mp3",
+        },
+        {
+            name:   "Prince Of Darkness",
+            url:    "/music/Phonk/Prince.mp3",
+        },
+        {
+            name:   "Avoid Me",
+            url:    "/music/Phonk/Avoid.mp3",
+        },
+        {
+            name:   "S.X.N.D. N.X.D.E.S.",
+            url:    "/music/Phonk/SendNudes.mp3",
+        },
+        {
+            name:   "Overdose",
+            url:    "/music/Phonk/Overdose.mp3",
+        },
+    ],
+    
+    hardObject : [
+        {
+            name: "BassSlut",
+            url: "/music/Hard/BassSlut.mp3",
+        },
+    ], 
+};
 
-const  phonkObject = [
-    {
-        name:   "Cruel",
-        url:    "/music/Phonk/Cruel.mp3",
-    },
-    {
-        name:   "Ragnarok",
-        url:    "/music/Phonk/Ragnarok.mp3",
-    },
-    {
-        name:   "Berserk",
-        url:    "/music/Phonk/Berserk.mp3",
-    },
-    {
-        name:   "Wrath",
-        url:    "/music/Phonk/Wrath.mp3",
-    },
-    {
-        name:   "Immaculate",
-        url:    "/music/Phonk/Immaculate.mp3",
-    },
-    {
-        name:   "Shadow",
-        url:    "/music/Phonk/Shadow.mp3",
-    },
-    {
-        name:   "Midnight",
-        url:    "/music/Phonk/Midnight.mp3",
-    },
-    {
-        name:   "Murder In My Mind",
-        url:    "/music/Phonk/MIMM.mp3",
-    },
-    {
-        name:   "Prince Of Darkness",
-        url:    "/music/Phonk/Prince.mp3",
-    },
-    {
-        name:   "Avoid Me",
-        url:    "/music/Phonk/Avoid.mp3",
-    },
-    {
-        name:   "S.X.N.D. N.X.D.E.S.",
-        url:    "/music/Phonk/SendNudes.mp3",
-    },
-    {
-        name:   "Overdose",
-        url:    "/music/Phonk/Overdose.mp3",
-    },
-];
+
 music.volume = 0.1;
-let selectedObject = phonkObject
-/* Selecting random music from array function */
+let selectedObject;
 
-function musicSelect(selectedObject) {
-    if(music.paused) {
-        const  rand = Math.floor(Math.random() * selectedObject.length);
-        source.src = selectedObject[rand].url;
-        music.load();
-        music.play();
-        console.log("Playing " + selectedObject[rand].name);
-    }
+/* Defining the music selecter function */
+
+const musicSelect = function(selectedObject) {
+    const  rand = Math.floor(Math.random() * selectedObject.length);
+    source.src = selectedObject[rand].url;
+    music.load();
+    music.play();
+    console.log("Playing " + selectedObject[rand].name);
+    document.getElementById("Title").innerHTML = "Playing: "+ selectedObject[rand].name;
 }
 
-/* Detecting user interaction to prevent user didn't interact with document error */
+const randomMusic = function() {
+    let keys = Object.keys(objects);
+    const rand = Math.floor(Math.random() * keys.length);
+    const randomObject = objects[keys[rand]];
+    randommMusic = Math.floor(Math.random() * randomObject.length);
+    source.src = randomObject[randommMusic].url;
+    music.load();
+    music.play();
+    console.log("Playing " + randomObject[randommMusic].name);
+    document.getElementById("Title").innerHTML = "Playing: "+ randomObject[randommMusic].name;
+}
 
-document.addEventListener("click", musicSelect(selectedObject));
+/* Button that plays new music */
+
+button.onclick = function(){
+    randomMusic();
+}
 
 /* If music ends calling the random music selecter and player function */
 
-music.addEventListener('pause', musicSelect(selectedObject));
+music.addEventListener('pause', function(){
+    musicSelect(selectedObject);
+});
 
-/* Adding Buttons 
+/* Adding Buttons (In Progress) */
 
 phonkButton.onclick = function(){
-    selectedObject = phonkObject
-    musicSelect(selectedObject)
+    selectedObject = objects["phonkObject"];
+    musicSelect(selectedObject);
 }
-*/
+
+hardButton.onclick = function() {
+    selectedObject = objects["hardObject"];
+    musicSelect(selectedObject);
+}
